0e75cf8ea4565d175f3ba253f64de900d7907217
Lizandra Malta <lizlrmalta@gmail.com>
2025-10-21 15:23:59 -0300

    first commit

---
+++ FakeNews/FakeNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeNewsApp.swift
+//  FakeNews
+//
+//  Created by Lizandra Malta on 21/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FakeNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

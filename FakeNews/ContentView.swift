6012ee355b0be93cdc6246a3a0e969f74a3471ac
Késia Silva <kesiasilvab6@gmail.com>
2025-10-23 15:28:02 -0300

    we adjusted the delete and the add and adjusted the front end

--- FakeNews/ContentView.swift
+++ FakeNews/ContentView.swift
@@ -1,136 +1,160 @@
 import SwiftUI
 
 struct ContentView: View {
-	
+    
     @Environment(NewsViewModel.self) private var vm
-	@Environment(\.dismiss) var dismiss
-	
-	@StateObject private var generator = NoticeGeneration()
-	@State private var isLoading = false
-	@State private var errorMessage: String?
-	@State private var selectedNotice: News?
-	
-	var body: some View {
-		NavigationStack {
-			VStack(spacing: 20) {
-                if vm.isLoading {
+    @Environment(\.dismiss) var dismiss
+    
+    @StateObject private var generator = NoticeGeneration()
+    @State private var isLoading = false
+    @State private var errorMessage: String?
+    @State private var selectedNotice: News?
+    
+    @State var flushUI: Bool = false
+    
+    var body: some View {
+        NavigationStack {
+            
+            if vm.isLoading {
+                VStack{
+                    Spacer()
                     ProgressView("Carregando...")
-                } else if vm.news.isEmpty {
-                    Text("Toque em 'Gerar Notícia' para começar.")
-                        .foregroundStyle(.secondary)
-                        .multilineTextAlignment(.center)
-                        .padding()
-				} else {
-					VStack(alignment: .leading, spacing: 10) {
-						Text("Notícias")
-							.font(.title2.bold())
-							.padding(.horizontal)
-						
-						ScrollView(.horizontal, showsIndicators: false) {
-							HStack(spacing: 20) {
-                                ForEach(vm.news) { notice in
-									Button {
-										selectedNotice = notice
-									} label: {
-										VStack(alignment: .leading, spacing: 8) {
-											Text(notice.title)
-												.font(.headline)
-                                                .fontWeight(.bold)
-												.foregroundStyle(.primary)
-												.lineLimit(2)
-											
-                                                .padding(.bottom, 3)
-                                            
-											Text(notice.description)
-												.font(.subheadline)
-												.foregroundStyle(.secondary)
-												.lineLimit(7)
-                                            
-                                            Spacer()
-										}
-                                        
-										.padding()
-										.frame(width: 300, height: 250 , alignment: .leading)
-										.background(Color(.systemGray6))
-										.cornerRadius(16)
-										.shadow(radius: 3, y: 2)
-									}
-									.buttonStyle(.plain)
-								}
-							}
-							.padding(.horizontal)
-						}
-					}
-				}
-				
-				// MARK: - Gráficos
-				VStack(alignment: .leading, spacing: 16) {
-					Text("Estatísticas")
-						.font(.title2.bold())
-						.padding(.horizontal)
-					
-					ScrollView(.horizontal, showsIndicators: false) {
-						HStack(spacing: 16) {
-							// Você pode adicionar vários gráficos aqui
-							ForEach(0..<3, id: \.self) { _ in
-								BarChartView()
-									.padding(10)
-									.frame(width: 300, height: 300)
-									.background(Color(.systemGray6))
-									.cornerRadius(16)
-									.shadow(radius: 3, y: 2)
-							}
-						}
-						.padding(.horizontal)
-					}
-				}
-				
-				if let error = errorMessage {
-					Text(error)
-						.foregroundColor(.red)
-						.font(.caption)
-				}
-				
-				Button(action: {
-					Task {
-						await generateNotice()
-					}
-				}) {
-					if isLoading {
-						ProgressView()
-					} else {
-						Text("Gerar Notícia Aleatória")
-							.fontWeight(.semibold)
-					}
-				}
-				.buttonStyle(.borderedProminent)
-				.disabled(isLoading)
-			}
-			.padding()
-			.navigationDestination(item: $selectedNotice) { notice in
-				NoticePageView(notice: notice)
-			}
-		}
-	}
-	
-	private func generateNotice() async {
-		isLoading = true
-		errorMessage = nil
-		
-		do {
-			let newNotice = try await generator.generateNoticeStreaming()
-			
+                    Spacer()
+                }
+            } else {
+                ScrollView {
+                    VStack(spacing: 20) {
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("Notícias")
+                                .font(.title2.bold())
+                            
+                            if vm.news.isEmpty {
+                                Text("Toque em 'Gerar Notícia' para começar.")
+                                    .foregroundStyle(.secondary)
+                                    .multilineTextAlignment(.center)
+                                    .padding()
+                            } else {
+                                ScrollView(.horizontal, showsIndicators: false) {
+                                    HStack(spacing: 20) {
+                                        ForEach(vm.news) { notice in
+                                            Button {
+                                                selectedNotice = notice
+                                            } label: {
+                                                VStack(alignment: .leading, spacing: 8) {
+                                                    Text(notice.title)
+                                                        .font(.headline)
+                                                        .fontWeight(.bold)
+                                                        .foregroundStyle(.primary)
+                                                        .lineLimit(2)
+                                                    
+                                                        .padding(.bottom, 3)
+                                                    
+                                                    Text(notice.description)
+                                                        .font(.subheadline)
+                                                        .foregroundStyle(.secondary)
+                                                        .lineLimit(7)
+                                                    
+                                                    Spacer()
+                                                }
+                                                
+                                                .padding()
+                                                .frame(width: 300, height: 250 , alignment: .leading)
+                                                .background(Color(.systemGray6))
+                                                .cornerRadius(16)
+                                                .shadow(radius: 3, y: 2)
+                                            }
+                                            .buttonStyle(.plain)
+                                        }
+                                    }
+                                    .padding(.horizontal)
+                                }
+                            }
+                            
+                        }
+                        
+                        // MARK: - Gráficos
+                        VStack(alignment: .leading, spacing: 16) {
+                            Text("Estatísticas")
+                                .font(.title2.bold())
+                                .padding(.horizontal)
+                            
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack(spacing: 16) {
+                                    // Você pode adicionar vários gráficos aqui
+                                    ForEach(0..<3, id: \.self) { _ in
+                                        BarChartView()
+                                            .padding(10)
+                                            .frame(width: 300, height: 300)
+                                            .background(Color(.systemGray6))
+                                            .cornerRadius(16)
+                                            .shadow(radius: 3, y: 2)
+                                    }
+                                }
+                                .padding(.horizontal)
+                            }
+                        }
+                        
+                        if let error = errorMessage {
+                            Text(error)
+                                .foregroundColor(.red)
+                                .font(.caption)
+                        }
+                        
+                        Button(action: {
+                            Task {
+                                await generateNotice()
+                            }
+                            
+                        }) {
+                            if isLoading {
+                                ProgressView()
+                            } else {
+                                Text("Gerar Notícia Aleatória")
+                                    .fontWeight(.semibold)
+                            }
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .disabled(isLoading)
+                    }
+                    
+                    .padding()
+                    .navigationDestination(item: $selectedNotice) { notice in
+                        NoticePageView(notice: notice)
+                    }
+                    
+                }
+                .refreshable {
+                    Task{
+                        await vm.refresh()
+                    }
+                }
+            }
+            
+            
+        }
+        
+    }
+    
+    private func generateNotice() async {
+        isLoading = true
+        errorMessage = nil
+        
+        do {
+            let newNotice = try await generator.generateNoticeStreaming()
+            
+            
             Task {
                 await vm.add(news: newNotice)
             }
-		} catch {
-			errorMessage = error.localizedDescription
-		}
-		
-		isLoading = false
-	}
+        } catch {
+            errorMessage = error.localizedDescription
+        }
+        
+        isLoading = false
+        
+    }
 }
 
 #Preview {
-	ContentView()
+    ContentView()
 }
--- FakeNews/ViewModel/NewsViewModel.swift
+++ FakeNews/ViewModel/NewsViewModel.swift
@@ -25,16 +25,26 @@ final class NewsViewModel {
         isLoading = true
         defer { isLoading = false }
         do {
+            try await Task.sleep(for: .seconds(2))
             news = try await repo.fetchAll()
         } catch {
             lastError = "❌ Erro ao buscar: \(error.localizedDescription)"
         }
     }
     
-    func add(news: News) async {
+//    func add(news: News) async {
+//        do {
+//            try await repo.add(news)
+//            await refresh()
+//        } catch {
+//            lastError = "❌ Erro ao criar: \(error.localizedDescription)"
+//        }
+//    }
+    
+    func add(news newItem: News) async {
         do {
-            try await repo.add(news)
-            await refresh()
+            news.insert(newItem, at: 0)
+            try await repo.add(newItem)
         } catch {
             lastError = "❌ Erro ao criar: \(error.localizedDescription)"
         }
@@ -42,25 +52,25 @@ final class NewsViewModel {
     
     // Tem essa opção que faz 2 requisições para a rede gasta mais bateria e processamento mas funciona normalmente.
     
-    func delete(news: News) async {
-        do {
-            try await repo.delete(news)
-              print("🗑️ Pedido de deleção enviado para \(news.id)")
-            await refresh()
-        } catch {
-              print("❌ Erro ao deletar:", error)
-            lastError = "❌ Erro ao deletar: \(error.localizedDescription)"
-        }
-    }
-    
-    // Esse faz apenas 1 requisição na rede e depois apaga da memoria do aparelho nao precisando fazer outra requisição e gastando bateria e processamento.
-    
-//    func delete(news itemDelete: News) async {
+//    func delete(news: News) async {
 //        do {
-//            try await self.repo.delete(itemDelete)
-//            news.removeAll { $0.id == itemDelete.id }
+//            try await repo.delete(news)
+//              print("🗑️ Pedido de deleção enviado para \(news.id)")
+//            await refresh()
 //        } catch {
+//              print("❌ Erro ao deletar:", error)
 //            lastError = "❌ Erro ao deletar: \(error.localizedDescription)"
 //        }
 //    }
+    
+    // Esse faz apenas 1 requisição na rede e depois apaga da memoria do aparelho nao precisando fazer outra requisição e gastando bateria e processamento.
+    
+    func delete(news itemDelete: News) async {
+        do {
+            try await self.repo.delete(itemDelete)
+            news.removeAll { $0.id == itemDelete.id }
+        } catch {
+            lastError = "❌ Erro ao deletar: \(error.localizedDescription)"
+        }
+    }
 }
